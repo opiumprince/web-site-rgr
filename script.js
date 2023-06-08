31879ec68ccd0fd55d21cf6f7702f494cd4d2155
opiumprince <128786059+opiumprince@users.noreply.github.com>
2023-06-08 09:32:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showText() {
+  var logoText = document.getElementById("logoText");
+  logoText.style.display = "none";
+}
+
+function hideText() {
+  var logoText = document.getElementById("logoText");
+  logoText.style.display = "block";
+}
